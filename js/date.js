4503ad0dbebe472fe4a29374f92f85f61df6657b
daniel65dj <dani65.dj@gmail.com>
2016-08-08 23:07:00 +0300

    dddd
    sss

--- js/date.js
+++
@@ -1,7 +0,0 @@
-
-let monthNames = new Array( "01","02","03","04","05","06","07","08","09","10","11","12");
-let now = new Date();
-thisYear = now.getYear();
-if(thisYear < 1900) {thisYear += 1900;} // corrections if Y2K display problem
-document.write(now.getDate() + "." + monthNames[now.getMonth()] + "." + thisYear + "&nbsp; �.");
-// -->
